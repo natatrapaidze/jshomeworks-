cff2682ae2615eeaa5543db70a653418378b5632
natatrapaidze <126957966+natatrapaidze@users.noreply.github.com>
2023-05-06 13:10:39 +0400

    davamate mesame davaleba

---
+++ homeworks N3/main.js
@@ -0,0 +1,86 @@
+
+/*
+Problem 5:
+
+create an array that holds numbers from [-100, 100]
+increment every single element by 1
+*/
+
+const number =[-100 , 100];
+console.log(number)
+console.log(number[0] - 1)
+console.log(number[1] + 1)
+
+/*
+Problem 6:
+
+cube every single element in previously created array
+
+ვერ მივხვდიი როგორ გავაკეთო 
+*/
+
+
+/*
+Problem 7:
+
+create an array that holds user names (at least 10); firstNames
+transform every element in the list (array) to lower case
+*/
+ 
+let userName = [ "John", "Jny", "Fred", "Doe", "Kim", "Jery", "Jo", "Jon","Joy","Jek"]
+console.log(userName)
+
+
+
+
+
+
+/*
+Problem 8:
+
+capitalize every element in previously created array
+
+ვერ მივხვდი კითხვას :(
+*/
+
+
+
+/*
+Problem 9:
+
+create an array that holds user last names (at least 10); lastNames
+transform every element in the list (array) to upper case
+*/
+ 
+
+const lastNames = ["Giorgadze","Saakashvili", "Lomidze", "Todua",
+ "Iashvili","Melia","Lomia", "Tortladva", "Tolordava","Toria"];
+console.log(lastNames.length) 
+for( let i = 0; i < lastNames.length; i++) {
+    console.log(lastNames[i])
+}
+
+
+
+/*
+Problem 10:
+
+create an array that will hold arrays of user names and last names; users
+user previously created arrays (firstNames and lastNames arrays) and join them
+to create new array like given in below example
+EXAMPLE:
+[
+    ["name", "last name"], 
+    ["name", "last name"]
+]
+*/
+
+
+
+/*
+Problem 11:
+
+remove every user from the previously created array (users) 
+that firstName starts with character 'a' 
+and lastName contains character 'i' 
+*/
